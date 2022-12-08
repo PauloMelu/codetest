dba075dd7af5bfb1c7dac71a8d811062a141c8b5
Ian Paulo <115437855+PauloMelu@users.noreply.github.com>
2022-12-08 11:16:33 +0800

    Update and rename Decrypt.c to decrypt.c

---
+++ decrypt.c
@@ -0,0 +1,27 @@
+//decrypt with incremental shifting
+#include <stdio.h>
+int main() {
+    char word[50];
+    int shift,incrementvalue,increment=0;
+    
+    printf("Enter a word to encrypt: ");
+    scanf("%[^\n]s",&word);
+    printf("Shift: ");
+    scanf("%d",&shift);
+    printf("Increment: ");
+    scanf("%d",&incrementvalue);
+    
+    int decryptedvalue;
+    for(int i=0;i<strlen(word);i++){
+        if(word[i]==' ')
+            printf(" ");
+        else{
+            decryptedvalue = tolower(word[i]) - shift - increment;
+            while(decryptedvalue<97)
+                decryptedvalue += 26;
+            printf("%c",decryptedvalue);
+        }
+        increment += incrementvalue;
+    }
+    return 0;
+}
