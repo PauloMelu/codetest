0192521e60ff7e2d09f4debb51937b0d835decd9
Ian Paulo <115437855+PauloMelu@users.noreply.github.com>
2022-12-08 11:16:05 +0800

    Update and rename Encrypt.c to encrypt.c

---
+++ encrypt.c
@@ -0,0 +1,27 @@
+//encrypt with incremental shifting
+#include <stdio.h>
+int main() {
+    char word[50];
+    int shift,incrementvalue,increment=0;
+    
+    printf("Enter a word to encrypt: ");
+    scanf("%[^\n]s",&word);
+    printf("Shift: ");
+    scanf("%d",&shift);
+    printf("Increment: ");
+    scanf("%d",&incrementvalue);
+    
+    int encryptedvalue;
+    for(int i=0;i<strlen(word);i++){
+        if(word[i]==' ')
+            printf(" ");
+        else{
+            encryptedvalue = tolower(word[i]) + shift + increment;
+            while(encryptedvalue>122)
+                encryptedvalue -= 26;
+            printf("%c",encryptedvalue);
+        }
+        increment += incrementvalue;
+    }
+    return 0;
+}
